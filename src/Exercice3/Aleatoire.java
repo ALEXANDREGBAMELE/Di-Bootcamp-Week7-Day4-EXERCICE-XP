bd2ceb602492b9914427814f174771c85be691df
Gbamele Alexandre <alexdelor2022@gmail.com>
2023-02-12 23:58:08 +0100

    exercise XP

---
+++ src/Exercice3/Aleatoire.java
@@ -0,0 +1,26 @@
+package Exercice3;
+
+/*
+*@Alexandre GBAMELE
+*/
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Aleatoire {
+
+    public static void randomNumber() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Entrer un nombre");
+        int X = sc.nextInt();
+
+        System.out.println("Entrer un nombre");
+        int N = sc.nextInt();
+        Random r = new Random();
+        int[] tab = new int[N];
+        for (int i = 0; i < N; i++) {
+            tab[i] = r.nextInt(X - 1) + 1;
+            System.out.println(tab[i]);
+        }
+    }
+}
